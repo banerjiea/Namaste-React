c0b1fd21cf419cf1c24ba700bc26aaba3f38fd65
banerjiea <soubhik.hzs@gmail.com>
2024-04-12 18:15:39 +0530

    Episode Five Ver One

---
+++ Episode 5_Let's Get Hooked/src/components/Body.js
@@ -0,0 +1,36 @@
+import RestaurantCard from "./RestaurantCard";
+import { resList } from "../utils/mockData";
+import { useState } from "react";
+
+const Body = () => {
+  // const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const arr = useState(resList);
+  //const [listOfRestaurants, setListOfRestaurants] = arr;
+  const listOfRestaurants = arr[0];
+  const setListOfRestaurants = arr[1];
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.3
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
