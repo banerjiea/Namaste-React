3ca2b3d9eeb2597214fb1332939ffb5247e8ac33
banerjiea <soubhik.hzs@gmail.com>
2024-05-27 18:08:44 +0530

    Episode Eight Ver One

---
+++ Episode 8_Let's Get Classy/src/components/Body.js
@@ -0,0 +1,94 @@
+import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurant] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+  //const arr = useState(resList);
+  //const [listOfRestaurants, setListOfRestaurants] = arr;
+  //const listOfRestaurants = arr[0];
+  //const setListOfRestaurants = arr[1];
+  console.log("Body Rendered");
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5558616&lng=88.3057061&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    //Optional Chaining
+    setListOfRestaurants(
+      json?.data.cards[2]?.card.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data.cards[2]?.card.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  //Conditional Redering
+  // if (listOfRestaurants.length === 0) {
+  //   return <Shimmer />;
+  // }
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              //filter the restaurant cards and update the UI
+              //search text
+              console.log(searchText);
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.3
+            );
+            setFilteredRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestaurants.map((restaurant) => (
+          <Link
+            className="res"
+            key={restaurant.info.id}
+            to={"/restaurants/" + restaurant.info.id}
+          >
+            <RestaurantCard resData={restaurant} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
